bdd4527634b3f12d036c111f5a5040c5ade5cd07
Ivan <ivan.suerop@gmail.com>
2023-11-27 23:07:47 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './css/App.css'
+import { Sidebar } from './components/Sidebar'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className="AppGlass">
+        <Sidebar />
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Data/Data.js
@@ -0,0 +1,28 @@
+import { UilEstate, UilClipboardAlt, UilPackage, UilChart, UilMapMarker, UilArrowCircleRight } from '@iconscout/react-unicons'
+
+export const SidebarData = [
+    {
+      icon: UilEstate,
+      heading: "Dashboard",
+    },
+    {
+      icon: UilPackage,
+      heading: 'Products'
+    },
+    {
+      icon: UilClipboardAlt,
+      heading: "Transactions",
+    },
+    {
+      icon: UilMapMarker,
+      heading: "Locations",
+    },
+    {
+      icon: UilArrowCircleRight,
+      heading: 'Actions'
+    },
+    {
+      icon: UilChart,
+      heading: 'Analytics'
+    },
+  ];
